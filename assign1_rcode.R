295742b39d105411addabe7c47dccb46af97623a
bkshaull <bkshaull@gmail.com>
2014-01-22 21:22:57 -0800

    Update assign1_rcode.R

--- assign1_rcode.R
+++ assign1_rcode.R
@@ -1,15 +1,10 @@
+setwd('/Volumes/MTN_LION_BU/Users/Shared/Delays1987_2013')
 time1=proc.time()
 freq=read.table("freq.txt", header= FALSE)
 numfreq=nrow(freq)
 freq$V2=as.integer(as.character(freq$V2)) #converting delay minutes into integers, originially stored differently
-freq=freq[1:numfreq,1:2]  #gets rid of the last two rows which contain NAs and the ArrDelay title
-
-for (j in 1:numfreq) {
-	if (is.na(freq[j,2])==TRUE){
-		freq[j,2]=0
-	}
-	
-} 
+freq[is.na(freq)]=0 #gets rid of the NAs
+freq=freq[order(freq$V2), ]
 
 #calculating Mean:
 sum_track=0
@@ -34,8 +29,8 @@ while (sum_v1<half) {
 median=freq[j+1,2]
 if (is.integer(half)==TRUE & sum_v1+freq[j+1,1]==half) {
         median=(freq[j+1,2]+freq[j+2,2])/2 }
-        
-        
+
+
 #calculating Standard deviation
 k=1
 sum_sq=0
@@ -43,8 +38,8 @@ while (k<=numfreq) {
         sum_sq=sum_sq+freq[k,1]*((freq[k,2]-mean)^2)
         k=k+1}
 st_dev=(sum_sq/(total_obs-1))^(1/2)
-proc.time()-time1
+time2=proc.time()-time1
 
-bshaull_assign1=list(time = time1, results = c(mean = mean, median = median, sd = st_dev),
+bshaull_assign1=list(time = time2, results = c(mean = mean, median = median, sd = st_dev),
      system = Sys.info(),  session = sessionInfo())
 save(bshaull_assign1, file="bshaull_assign1.rda")
